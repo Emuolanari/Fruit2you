da60f0b8793a8eec1042e51e5ef5a5beae9a4a5f
Emuolanari <emuolao@gmail.com>
2020-07-01 11:49:21 +0100

    bottom navigation view

--- app/src/main/java/com/app/fruit2you/MainActivity.kt
+++ app/src/main/java/com/app/fruit2you/MainActivity.kt
@@ -11,10 +11,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        signout.setOnClickListener {
+       /* signout.setOnClickListener {
             FirebaseAuth.getInstance().signOut()
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
-        }
+        }*/
     }
 }
