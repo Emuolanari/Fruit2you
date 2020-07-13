4a346d1661a31edccb9de0e5a9036fce2cd374bf
Emuolanari <emuolao@gmail.com>
2020-07-13 21:55:38 +0100

    updated ui

--- app/src/main/java/com/app/fruit2you/MainActivity.kt
+++ app/src/main/java/com/app/fruit2you/MainActivity.kt
@@ -34,7 +34,7 @@ class MainActivity : AppCompatActivity() {
     private fun setCurrentFragment(fragment: Fragment){
         supportFragmentManager.beginTransaction().apply{
             replace(R.id.flFragment, fragment)
-            //addToBackStack(null)
+            addToBackStack(null)
             commit()
         }
     }
