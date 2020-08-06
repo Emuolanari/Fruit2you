5c1398e53f41be85eda41596f017b941ab5bfb67
Emuolanari <emuolao@gmail.com>
2020-08-06 19:16:37 +0100

    add items to cart button

--- app/src/main/java/com/app/fruit2you/ui/fragments/CartFragment.kt
+++ app/src/main/java/com/app/fruit2you/ui/fragments/CartFragment.kt
@@ -1,8 +1,10 @@
 package com.app.fruit2you.ui.fragments
 
 import android.os.Bundle
+import android.text.TextUtils.replace
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
 import androidx.lifecycle.ViewModelProviders
 import com.app.fruit2you.ui.Fruit2YouViewModel
 import com.app.fruit2you.ui.Fruit2YouViewModelFactory
@@ -15,6 +17,7 @@ import org.kodein.di.generic.instance
 import androidx.lifecycle.Observer
 import androidx.lifecycle.observe
 import com.app.fruit2you.data.database.entities.FruitItem
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.cart_fragment.*
 
 
@@ -26,14 +29,37 @@ class CartFragment: Fragment(R.layout.cart_fragment), KodeinAware {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val homeFragment = HomeFragment()
         val viewModel = ViewModelProviders.of(this, factory).get(Fruit2YouViewModel::class.java)
         val adapter = FruitsAdapter(listOf(), viewModel)
         cartRecyclerView.layoutManager = LinearLayoutManager(activity)
         cartRecyclerView.adapter = adapter
 
+        fun setCurrentFragment(fragment: Fragment){
+            parentFragmentManager?.beginTransaction()?.apply{
+                replace(R.id.flFragment, homeFragment)
+                addToBackStack(null)
+                commit()
+            }
+        }
+        viewModel.numberOfCartItems().observe(viewLifecycleOwner, Observer <Int> {
+
+            val x = it
+            if (x==null){
+                checkout.visibility =  View.GONE
+                addItems.visibility = View.VISIBLE
+            }
+        })
+
+        addItems.setOnClickListener {
+            setCurrentFragment(homeFragment)
+        }
+
+
         viewModel.getAllShoppingItems().observe(viewLifecycleOwner, Observer <List<FruitItem>> {
             adapter.items=it
             adapter.notifyDataSetChanged()
         })
+
     }
 }
