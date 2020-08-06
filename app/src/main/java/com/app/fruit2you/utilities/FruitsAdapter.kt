b73567655e5dee62c455edb8b74c3c3b0bd748d2
Emuolanari <emuolao@gmail.com>
2020-08-06 21:39:52 +0100

    bottom navigation redesigned and images added to cart recyclerview

--- app/src/main/java/com/app/fruit2you/utilities/FruitsAdapter.kt
+++ app/src/main/java/com/app/fruit2you/utilities/FruitsAdapter.kt
@@ -8,6 +8,7 @@ import com.app.fruit2you.R
 import com.app.fruit2you.data.database.entities.FruitItem
 import com.app.fruit2you.ui.Fruit2YouViewModel
 import kotlinx.android.synthetic.main.fruit_item.view.*
+import kotlinx.android.synthetic.main.fruit_item.view.tvQuantity
 
 class FruitsAdapter(
     var items: List<FruitItem>,
@@ -25,10 +26,23 @@ class FruitsAdapter(
 
     override fun onBindViewHolder(holder: FruitsViewHolder, position: Int) {
         val curFruitItem = items[position]
-
+        val  myImage = holder.itemView.fruitImage
         holder.itemView.tvName.text = curFruitItem.name
         holder.itemView.tvQuantity.text = "${curFruitItem.quantity}"
         holder.itemView.amount.text = "${curFruitItem.amount}"
+
+        if(curFruitItem.name=="strawberry pack"){
+            myImage.setImageResource(R.drawable.strawberry)
+        }
+        else if(curFruitItem.name=="coconut pack"){
+            myImage.setImageResource(R.drawable.coconut)
+        }
+        else if(curFruitItem.name=="mangoes pack"){
+            myImage.setImageResource(R.drawable.mangoes)
+        }
+        else{
+            myImage.setImageResource(R.drawable.oranges)
+        }
 
         holder.itemView.ivDelete.setOnClickListener {
             viewModel.delete(curFruitItem)
