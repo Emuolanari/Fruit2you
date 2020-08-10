a101e38fa627b7bc5c2777a307761d11327ae746
Emuolanari <emuolao@gmail.com>
2020-08-10 23:20:10 +0100

    profile working perfectly

--- app/src/main/java/com/app/fruit2you/ui/ChangeDetails.kt
+++ app/src/main/java/com/app/fruit2you/ui/ChangeDetails.kt
@@ -3,9 +3,12 @@ package com.app.fruit2you.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.TextUtils
+import android.util.Log
 import android.widget.Toast
 import androidx.lifecycle.LifecycleCoroutineScope
 import com.app.fruit2you.R
+import com.google.firebase.auth.AuthCredential
+import com.google.firebase.auth.EmailAuthProvider
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.EventListener
@@ -17,6 +20,8 @@ import java.util.*
 class ChangeDetails : AppCompatActivity() {
     private lateinit var auth: FirebaseAuth
     private lateinit var fstore: FirebaseFirestore
+    private lateinit var emailText: String
+    private lateinit var passWord: String
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -35,64 +40,63 @@ class ChangeDetails : AppCompatActivity() {
                 nameField.setText(snapshot?.getString("fName"))
                 emailField.setText(snapshot?.getString("email"))
                 phoneField.setText(snapshot?.getString("phone"))
+                emailText = snapshot?.getString("email").toString().trim()
+                passWord = snapshot?.getString("password").toString().trim()
 
+            }
+        }
 
-                update.setOnClickListener {
-                    val newName = nameField.text.toString().trim().toUpperCase(Locale.getDefault())
-                    val newPhone = phoneField.text.toString().trim()
-                    val newEmail = emailField.text.toString().trim()
-                    if (newName.isEmpty()||!newName.matches(nameRegex)){
-                        nameField.error = "Please enter your full name"
-                        return@setOnClickListener
-                    }
-                    if (newPhone.isEmpty()||!newPhone.matches(phoneRegex)){
-                        phoneField.error = "Please enter your correct phone number"
-                        return@setOnClickListener
-                    }
+        update.setOnClickListener {
+            val newName = nameField.text.toString().trim().toUpperCase(Locale.getDefault())
+            val newPhone = phoneField.text.toString().trim()
+            val newEmail = emailField.text.toString().trim()
+            if (newName.isEmpty()||!newName.matches(nameRegex)){
+                nameField.error = "Please enter your full name"
+                return@setOnClickListener
+            }
+            if (newPhone.isEmpty()||!newPhone.matches(phoneRegex)){
+                phoneField.error = "Please enter your correct phone number"
+                return@setOnClickListener
+            }
 
-                    if(newEmail.isEmpty() || !android.util.Patterns.EMAIL_ADDRESS.matcher(newEmail).matches()){
-                        emailField.error = "Please enter your valid email address"
-                        return@setOnClickListener
-                    }
+            if(newEmail.isEmpty() || !android.util.Patterns.EMAIL_ADDRESS.matcher(newEmail).matches()){
+                emailField.error = "Please enter your valid email address"
+                return@setOnClickListener
+            }
 
-                    else{
-                        val userId = auth.currentUser!!.uid
-                        val documentRef = fstore.collection("users").document(userId)
-                        documentRef.addSnapshotListener {snapshot, e->
-                            val email = snapshot?.getString("email")
-                            val password = snapshot?.getString("password")
-                            if (email!=null && password!=null){
-                                auth.signInWithEmailAndPassword(email,password)
-                                    .addOnCompleteListener { task ->
-                                        if (task.isSuccessful) {
-                                            // Sign in success now update email
-                                            auth.currentUser!!.updateEmail(newEmail)
-                                                .addOnCompleteListener{ task ->
-                                                    if (task.isSuccessful) {
-                                                        val user = hashMapOf<String, Any>()
-                                                        user["fName"] = newName
-                                                        user["email"] = newEmail
-                                                        user["phone"] = newPhone
+            else{
+                val  thisUser = auth.currentUser
+                val userId = auth.currentUser!!.uid
+                val credential: AuthCredential = EmailAuthProvider
+                    .getCredential(emailText, passWord)
+                
+                thisUser?.reauthenticate(credential)?.addOnCompleteListener {task->
 
-                                                        val documentReference = fstore.collection("users").document(userId)
-                                                        documentReference.set(user, SetOptions.merge())
-                                                        Toast.makeText(this,"Details updated successfully", Toast.LENGTH_SHORT).show()
-                                                        return@addOnCompleteListener
-                                                    }else{
-                                                        // email update failed
-                                                        Toast.makeText(this,"Please check details and try again", Toast.LENGTH_SHORT).show()
-                                                    }
-                                                }
-                                        } else {
-                                            Toast.makeText(applicationContext,"Authentication failed", Toast.LENGTH_LONG).show()
-                                        }
-                                    }
+                    if (task.isSuccessful) {
+                        thisUser.updateEmail(newEmail).addOnCompleteListener {task->
+                            if (task.isSuccessful) {
+                                val user = hashMapOf<String, Any>()
+                                user["fName"] = newName
+                                user["email"] = newEmail
+                                user["phone"] = newPhone
 
+                                val documentReference = fstore.collection("users").document(userId)
+                                documentReference.set(user, SetOptions.merge())
+                                Toast.makeText(this,"Details updated successfully",Toast.LENGTH_SHORT)
+                                    .show()
+                            } else {
+                                Toast.makeText(this,"Error, details not updated, check details and try again",
+                                    Toast.LENGTH_SHORT)
+                                    .show()
                             }
 
                         }
+                    } else {
+                        Toast.makeText(this,"Authentication failed",
+                            Toast.LENGTH_SHORT)
+                            .show()
                     }
-                    return@setOnClickListener
+
                 }
             }
         }
--- app/src/main/java/com/app/fruit2you/ui/UpdatePassword.kt
+++ app/src/main/java/com/app/fruit2you/ui/UpdatePassword.kt
@@ -3,8 +3,11 @@ package com.app.fruit2you.ui
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import com.app.fruit2you.R
+import com.google.firebase.auth.AuthCredential
+import com.google.firebase.auth.EmailAuthProvider
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.SetOptions
@@ -15,7 +18,7 @@ import kotlinx.android.synthetic.main.update_password.update
 class UpdatePassword : AppCompatActivity() {
     private lateinit var auth: FirebaseAuth
     private lateinit var fstore: FirebaseFirestore
-    private lateinit var x: String
+    private lateinit var emailText: String
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.update_password)
@@ -26,8 +29,9 @@ class UpdatePassword : AppCompatActivity() {
             val userId = auth.currentUser!!.uid
             val documentRef = fstore.collection("users").document(userId)
             documentRef.addSnapshotListener {snapshot, e->
-                val fstorePassword = snapshot?.getString("password").toString().trim()
-                x = fstorePassword
+                //val fstorePassword = snapshot?.getString("password").toString().trim()
+                val email = snapshot?.getString("email").toString().trim()
+                emailText = email
             }
 
 
@@ -35,53 +39,42 @@ class UpdatePassword : AppCompatActivity() {
                 val oldPassword = currentPassword.text.toString().trim()
                 val updatedPassword = newPassword.text.toString().trim()
 
-                if(oldPassword!=x){
-                    currentPassword.error = "Old password incorrect"
-                    return@setOnClickListener
-                }
                 if(updatedPassword.length<6){
                     newPassword.error = "Password must be at least 6 characters"
                     return@setOnClickListener
                 }
 
                 else{
-                    val userID = auth.currentUser!!.uid
-                    val documentRef = fstore.collection("users").document(userID)
-                    documentRef.addSnapshotListener { snapshot, e ->
-                        val email = snapshot?.getString("email")
-                        val password = snapshot?.getString("password")
+                    val  thisUser = auth.currentUser
+                    val credential: AuthCredential = EmailAuthProvider
+                            .getCredential(emailText, oldPassword)
 
-                        if (email != null && password != null) {
-                            auth.signInWithEmailAndPassword(email, password)
-                                .addOnCompleteListener { task ->
+                    // Prompt the user to re-provide their sign-in credentials
+                    thisUser?.reauthenticate(credential)?.addOnCompleteListener {task->
+
+                        if (task.isSuccessful) {
+                            thisUser.updatePassword(updatedPassword).addOnCompleteListener {task->
                                     if (task.isSuccessful) {
-                                        auth.currentUser!!.updatePassword(updatedPassword)
-                                            .addOnCompleteListener { task ->
-                                                if (task.isSuccessful) {
-                                                    val user = hashMapOf<String, Any>()
-                                                    user["password"] = updatedPassword
+                                        val user = hashMapOf<String, Any>()
+                                        user["password"] = updatedPassword
+                                        val documentReference = fstore.collection("users").document(userId)
+                                        documentReference.set(user, SetOptions.merge())
 
-                                                    val documentReference =
-                                                        fstore.collection("users").document(userID)
-                                                    documentReference.set(user, SetOptions.merge())
-                                                    Toast.makeText(applicationContext, "Password updated successfully",
-                                                        Toast.LENGTH_SHORT
-                                                    ).show()
-                                                } else {
-                                                    // password update failed
-                                                    Toast.makeText(
-                                                        applicationContext,
-                                                        "Please check details and try again",
-                                                        Toast.LENGTH_SHORT
-                                                    ).show()
-                                                }
-                                            }
+                                        Toast.makeText(this,"Password updated successfully",Toast.LENGTH_SHORT)
+                                            .show()
                                     } else {
-                                        Toast.makeText(applicationContext, "Authentication failed",
-                                            Toast.LENGTH_LONG).show()
+                                        Toast.makeText(this,"Password not updated, check details and try again",
+                                            Toast.LENGTH_SHORT)
+                                            .show()
                                     }
-                                }
+
+                            }
+                        } else {
+                            Toast.makeText(this,"Authentication failed",
+                                Toast.LENGTH_SHORT)
+                                .show()
                         }
+
                     }
 
                 }
