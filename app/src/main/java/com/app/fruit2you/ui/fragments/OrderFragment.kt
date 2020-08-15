1ac3eb339439a98f0eb245989ad44d360e568db1
Emuolanari <emuolao@gmail.com>
2020-08-15 23:08:03 +0100

    order screen styled

--- app/src/main/java/com/app/fruit2you/ui/fragments/OrderFragment.kt
+++ app/src/main/java/com/app/fruit2you/ui/fragments/OrderFragment.kt
@@ -16,6 +16,8 @@ import com.app.fruit2you.ui.Fruit2YouViewModelFactory
 import com.app.fruit2you.ui.LoginActivity
 import com.flutterwave.raveandroid.rave_java_commons.Meta
 import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.order_fragment.*
 import org.kodein.di.KodeinAware
 import org.kodein.di.android.x.kodein
 import org.kodein.di.generic.instance
@@ -30,18 +32,30 @@ class OrderFragment: Fragment(R.layout.order_fragment), KodeinAware {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val viewModel = ViewModelProvider(this, factory).get(Fruit2YouViewModel::class.java)
-        //Log.d("OrdersAct", viewModel.getAllShoppingItems().toString())
-        viewModel.getAllShoppingItems().observe(viewLifecycleOwner, Observer <List<FruitItem>> {
-            val items = it
-            val separator = ","
-            val itemsString = items.joinToString(separator)
-            val sdf = SimpleDateFormat("dd/M/yyyy hh:mm:ss", Locale.UK)
-            val currentDate = sdf.format(Date())
-            check = itemsString
-            Toast.makeText(activity,itemsString, Toast.LENGTH_LONG).show()
-            Toast.makeText(activity,currentDate, Toast.LENGTH_LONG).show()
-        })
+        val homeFragment = HomeFragment()
+        val cartFragment = CartFragment()
 
+        orderButton.setOnClickListener {
+            viewModel.numberOfCartItems().observe(viewLifecycleOwner, Observer <Int> {
+                val x = it
+                if (x==null){
+                    setCurrentFragment(homeFragment)
+                    activity?.bottomNavigationView?.selectedItemId =  R.id.miHome
+                }
+                else{
+                    setCurrentFragment(cartFragment)
+                    activity?.bottomNavigationView?.selectedItemId =  R.id.miCart
+                }
+            })
+        }
+    }
+
+    private fun setCurrentFragment(fragment: Fragment){
+        parentFragmentManager.beginTransaction().apply{
+            replace(R.id.flFragment, fragment)
+            addToBackStack(null)
+            commit()
+        }
     }
 
 
