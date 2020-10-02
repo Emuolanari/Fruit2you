e5ac55db65d839c4eebc53f15ff615a954d8c775
Emuolanari <emuolao@gmail.com>
2020-10-02 18:13:59 +0100

    cart working

--- app/src/main/java/com/app/fruit2you/other/FruitsAdapter.kt
+++ app/src/main/java/com/app/fruit2you/other/FruitsAdapter.kt
@@ -7,8 +7,11 @@ import androidx.recyclerview.widget.RecyclerView
 import com.app.fruit2you.R
 import com.app.fruit2you.data.database.entities.FruitItem
 import com.app.fruit2you.ui.Fruit2YouViewModel
+import com.bumptech.glide.Glide
+import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.fruit_item.view.*
 import kotlinx.android.synthetic.main.fruit_item.view.tvQuantity
+import java.net.URL
 
 class FruitsAdapter(
     var items: List<FruitItem>,
@@ -30,19 +33,16 @@ class FruitsAdapter(
         holder.itemView.tvName.text = curFruitItem.name
         holder.itemView.tvQuantity.text = "${curFruitItem.quantity}"
         holder.itemView.amount.text = "${curFruitItem.amount}"
+        var fstore: FirebaseFirestore = FirebaseFirestore.getInstance()
+        val docRef = fstore.collection("items").document(curFruitItem.name)
+        docRef.addSnapshotListener{snapshot, e->
+            val imageSrc = snapshot?.getString("imageURL")
+            val url = URL(imageSrc)
+            Glide.with(holder.itemView.context)
+                .load(url)
+                .into(myImage)
+        }
 
-        if(curFruitItem.name=="Strawberries pack"){
-            myImage.setImageResource(R.drawable.strawberry)
-        }
-        else if(curFruitItem.name=="Coconuts pack"){
-            myImage.setImageResource(R.drawable.coconut)
-        }
-        else if(curFruitItem.name=="Mangoes pack"){
-            myImage.setImageResource(R.drawable.mangoes)
-        }
-        else{
-            myImage.setImageResource(R.drawable.oranges)
-        }
 
         holder.itemView.ivDelete.setOnClickListener {
             viewModel.delete(curFruitItem)
